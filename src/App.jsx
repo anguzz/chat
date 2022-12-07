0e03eb56eabe53aa811c41c49fc9ab581538081d
Angel <santoyox714@gmail.com>
2022-12-06 23:47:59 -0800

    add pockethost url
    -leave local testing as seperate script npm local

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 
 
-import {client, authData} from './utils/config'
+import {client} from './utils/config'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p class="text-3xl font-bold underline">
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-3xl font-bold underline">
+          hello
         </p>
-        <h1>Hello, {authData.admin.email}</h1>
-        
+    
       </header>
     </div>
   );
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,17 @@
 import Pocketbase from 'pocketbase';
 
-export const USERNAME = "santoyox714@gmail.com";
+export const url='https://angus.pockethost.io/';
+export const client = new Pocketbase(url);
+
+//the following is for local testing
+//uncomment below and use npm local to start local pocketbase exe 
+//use same username/pw below at  http://127.0.0.1:8090/_/ 
+/*
+
+export const USERNAME = "email@email.com";
 export const PASSWORD = "devTest1234";
 export const url='http://127.0.0.1:8090';
-
-export const client = new Pocketbase(url);
 export const authData =  await client.admins.authWithPassword(USERNAME, PASSWORD)
+*/
+
 
