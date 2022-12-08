9dd4deb1d02b95adaf428d41fb9dc7763d6f0e54
Angel <santoyox714@gmail.com>
2022-12-07 19:03:23 -0800

    updates
    -change local testing script by getting rid of craco
    -change pocketbase client version

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,17 +1,21 @@
 import Pocketbase from 'pocketbase';
 
-export const url='https://angus.pockethost.io/';
+//using remote pockethost
+const url='https://angus.pockethost.io/'
 export const client = new Pocketbase(url);
 
-//the following is for local testing
-//uncomment below and use npm local to start local pocketbase exe 
-//use same username/pw below at  http://127.0.0.1:8090/_/ 
-/*
 
-export const USERNAME = "email@email.com";
-export const PASSWORD = "devTest1234";
-export const url='http://127.0.0.1:8090';
-export const authData =  await client.admins.authWithPassword(USERNAME, PASSWORD)
+//local testing
+//use test@email.com and test123456 to login to local admin view 
+//comment out url above and uncomment the url below
+/*
+const local='http://127.0.0.1:8090';
+export const client = new Pocketbase(local);
 */
+
+
+
+
+
 
 
