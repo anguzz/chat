2fbda55af863a05339a739ca5979f02114ccc6b3
Angel <santoyox714@gmail.com>
2022-12-07 19:05:01 -0800

    Update config.js
    comments

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,9 +5,11 @@ const url='https://angus.pockethost.io/'
 export const client = new Pocketbase(url);
 
 
-//local testing
+//local testing steps
+//use 'npm run local' go to http://127.0.0.1:8090/_/ for admin collections view
 //use test@email.com and test123456 to login to local admin view 
 //comment out url above and uncomment the url below
+
 /*
 const local='http://127.0.0.1:8090';
 export const client = new Pocketbase(local);
