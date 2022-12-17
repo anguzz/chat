1d615486412e291ea7c4d9ac57a4179f938411dc
Donald Novasky <dnovasky@hotmail.com>
2022-12-17 15:58:21 -0800

    ran standard --fix

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,15 +2,14 @@ import Pocketbase from 'pocketbase'
 
 // To use with remote pockethost hosting uncomment the following  two lines
 // Use 'npm run pockethost'
-// const url = 'https://angus.pockethost.io/'
-// export const client = new Pocketbase(url)
+const url = 'https://angus.pockethost.io/'
+export const client = new Pocketbase(url)
 
 // To use with local pocketbase
 // use 'npm start' go to http://127.0.0.1:8090/_/ for admin collections view
 // use test@email.com and test123456 to login to local admin view
-// comment out url above and uncomment the url below
 // There are two test user in pocketbase. testUser1 and testUser2
 // The password for both is 12345678
 
-const local = 'http://127.0.0.1:8090'
-export const client = new Pocketbase(local)
+// const local = 'http://127.0.0.1:8090'
+// export const client = new Pocketbase(local)
